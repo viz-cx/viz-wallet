b3173e6312c657d6938c8445f022adc5f53fc4c0
Vladimir Babin <vovababin@gmail.com>
2021-02-21 11:48:59 +0500

    Initial Commit

---
+++ Shared/viz_walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  viz_walletApp.swift
+//  Shared
+//
+//  Created by Бабин Владимир on 21.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct viz_walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
