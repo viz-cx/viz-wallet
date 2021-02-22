a2cb998a5baaec5ff8c9e08848d628667b2c98ba
Vladimir Babin <vovababin@gmail.com>
2021-02-23 01:19:30 +0500

    Add login function

--- Shared/UserAuth.swift
+++ Shared/UserAuth.swift
@@ -6,14 +6,40 @@
 //
 
 import Combine
+import Foundation
+import VIZ
 
 class UserAuth: ObservableObject {
     
     let objectWillChange = PassthroughSubject<UserAuth,Never>()
     
-    func login() {
-        // login request... on success:
-        self.isLoggedin = true
+    func login(login: String, regularKey: String) {
+        guard login.count > 1 else {
+            print("Login too small")
+            return
+        }
+        let client = VIZ.Client(address: URL(string: "https://node.viz.cx")!)
+        let req = API.GetAccounts(names: [login])
+        let result = try? client.sendSynchronous(req)
+        guard let account = result?.first else {
+            print("Account not found")
+            return
+        }
+        var isRegularValid = false
+        for accountAuth in account.regularAuthority.keyAuths {
+            if accountAuth.weight >= account.regularAuthority.weightThreshold {
+                guard let publicKey = PrivateKey(regularKey)?.createPublic() else {
+                    continue
+                }
+                isRegularValid = publicKey.address == accountAuth.value.address
+                if isRegularValid {
+                    break
+                }
+            }
+        }
+        if isRegularValid {
+            self.isLoggedin = true
+        }
     }
     
     var isLoggedin = false {
