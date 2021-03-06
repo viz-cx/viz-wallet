f45d543f1cbe6c6eef9b8cb7eb63894e206bab89
Vladimir Babin <vovababin@gmail.com>
2021-03-06 21:13:50 +0500

    Add QR code to receive view

--- Shared/Views/ReceiveView.swift
+++ Shared/Views/ReceiveView.swift
@@ -5,14 +5,55 @@
 //  Created by Vladimir Babin on 28.02.2021.
 //
 
+import Foundation
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct ReceiveView: View {
+    private let context = CIContext()
+    private let filter = CIFilter.qrCodeGenerator()
+    
+    @EnvironmentObject private var userAuth: UserAuth
+    
     var body: some View {
-        LottieView(name:"31675-programming")
-            .background(
-                LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .top, endPoint: .bottom)
-                    .edgesIgnoringSafeArea(.all))
+        VStack {
+            Text("Login: \(userAuth.login)")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(
+                    maxWidth: .infinity,
+                    minHeight: 50,
+                    maxHeight: 50,
+                    alignment: .center
+                )
+                .background(Color.green)
+                .cornerRadius(15.0)
+                .onTapGesture {
+                    UIPasteboard.general.string = userAuth.login
+                }
+            
+            Spacer()
+            
+            Image(uiImage: genrateQRImage(text: userAuth.login))
+                .interpolation(.none)
+                .resizable()
+                .frame(maxHeight: 300, alignment: .center)
+                .aspectRatio(1, contentMode: .fit)
+            
+            Spacer()
+        }
+    }
+    
+    func genrateQRImage(text: String) -> UIImage {
+        let data = Data(text.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        if let qrCodeImage = filter.outputImage {
+            if let qrCodeCGImage = context.createCGImage(qrCodeImage, from: qrCodeImage.extent) {
+                return UIImage(cgImage: qrCodeCGImage)
+            }
+        }
+        return UIImage(systemName: "xmark") ?? UIImage()
     }
 }
 
