0a5ccf0f34a6735f5123da2fe0dfedf3b34ca2ba
Vladimir Babin <vovababin@gmail.com>
2022-01-02 14:45:22 +0500

    Add error descriptions

--- Shared/Data/Errors.swift
+++ Shared/Data/Errors.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
-enum Errors: Error {
+enum Errors: Error, LocalizedError {
     case UnknownError
     case SignError
     case KeyValidationError
     case LoginTooSmall
+    
+    var errorDescription: String? {
+        switch self {
+        case .UnknownError:
+            return NSLocalizedString("Unknown error", comment: "")
+        case .SignError:
+            return NSLocalizedString("Signature error", comment: "")
+        case .KeyValidationError:
+            return NSLocalizedString("Wrong key", comment: "")
+        case .LoginTooSmall:
+            return NSLocalizedString("Login too small", comment: "")
+        }
+    }
 }
